04c49339babc5375a3aecec634aac168fbc1f50a
“Tayyab <tayyabhaseeb5@gmail.com>
2025-04-18 20:34:02 +0100

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex h-24 bg-light-green">
+      <img src="/logo.png" height={64} className="h-16 m-auto" />
+    </header>
+  );
+}
+
+export default Header;
