e34da13a6d0cbfe55c37ff0b041c06d4d1bdf4df
“Tayyab <tayyabhaseeb5@gmail.com>
2025-04-18 23:16:52 +0100

    adds the action cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import Header from "./components/Header";
+import { MdOutlineAddCircleOutline } from "react-icons/md";
+import { MdPersonAddAlt } from "react-icons/md";
+import { GoPeople } from "react-icons/go";
+import ActionCard from "./components/ActionCard";
 
 export default function App() {
   return (
     <main className="flex flex-col">
       <Header />
+      <div className=" px-2 py-4 flex items-center justify-center gap-4">
+        <ActionCard
+          icon={<MdOutlineAddCircleOutline />}
+          label={"New Project"}
+        />
+        <ActionCard icon={<MdPersonAddAlt />} label={"Add New Members"} />
+        <ActionCard icon={<GoPeople />} label={"Team Members"} />
+      </div>
     </main>
   );
 }
